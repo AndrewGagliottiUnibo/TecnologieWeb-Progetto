b1cfcefe024b2b68f8b655539a7000626599a229
SalvatoreBennici <salvatore.bennici@studio.unibo.it>
2022-01-31 13:05:54 +0100

    Adding php template

---
+++ codice/template/navbar.php
@@ -0,0 +1,34 @@
+<header>
+    <h1>CacPlus.inc</h1>
+    <a href="index.html" title="Logo">
+        <img src="../immagini/logo.png" alt="Home">
+    </a>
+</header>
+<nav>
+    <ul>
+        <li class="nav_Catalogo">
+            <a href="#">Catalogo</a>
+            <!-- Nuova pagina -->
+        </li>
+        <li class="nav_Carrello">
+            <a href="#">Carrello</a>
+            <!-- Nuova pagina -->
+        </li>
+        <li class="nav_Login">
+            <a href="#">Login</a>
+            <!-- Nuova pagina -->
+        </li>
+        <li class="nav_Notifiche">
+            <a href="#">Notifiche</a>
+            <!-- Nuova pagina -->
+        </li>
+        <li class="nav_Info">
+            <a href="#">Info e Contatti</a>
+            <!-- Nuova pagina -->
+        </li>
+        <li class="nav_Impostazioni">
+            <a href="#">Impostazioni</a>
+            <!-- Nuova pagina -->
+        </li>
+    </ul>
+</nav>
