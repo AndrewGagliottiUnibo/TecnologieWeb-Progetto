96c318a8629516ff698923d3f126c6ce01d9bd02
SalvatoreBennici <salvatore.bennici@studio.unibo.it>
2022-02-02 18:02:42 +0100

    First navbar concept done

--- codice/script/navbar.js
+++ codice/script/navbar.js
@@ -1,9 +1,9 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("sidenav").style.width = "40%";
-  }
+  document.getElementById("sidenav").style.width = "70%";
+}
   
   /* Set the width of the side navigation to 0 */
 function closeNav() {
-    document.getElementById("sidenav").style.width = "0";
-  }
+  document.getElementById("sidenav").style.width = "0%";
+}
