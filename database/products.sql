a5e21be45adf6e4944c9f01e64ae6c36e2d4d5b5
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2022-02-06 18:10:05 +0100

    highlited with old db

--- database/products.sql
+++ database/products.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Feb 04, 2022 alle 17:30
+-- Creato il: Feb 06, 2022 alle 18:08
 -- Versione del server: 10.4.21-MariaDB
 -- Versione PHP: 8.0.12
 
@@ -32,24 +32,25 @@ CREATE TABLE `products` (
   `name` varchar(255) NOT NULL,
   `price` decimal(10,2) NOT NULL,
   `image` varchar(255) NOT NULL,
-  `description` text NOT NULL
+  `description` text NOT NULL,
+  `highlighted` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dump dei dati per la tabella `products`
 --
 
-INSERT INTO `products` (`id`, `name`, `price`, `image`, `description`) VALUES
-(1, 'Cactus con miccia', '2.50', '/TecnologieWeb/immagini/cactusMiccia.png', 'Cactus di porcellana attaccata ad una corda e pieno di polvere nera. Se lanciato alle spalle, può ostacolare gli inseguitori.\r\n\r\nE\' identico ad un comune cactus, tranne per la direzione in cui viene lanciato.'),
-(2, 'Cactus morbidoso (Verde)', '10.00', '/TecnologieWeb/immagini/morbidosoVerde.png', 'Chi non vorrebbe abbracciare un cactus e sentirsi al sicuro da ogni pericolo? Ora puoi!\r\n\r\nATTENZIONE: non è garantita la totale assenza di spine, tenere lontano dalla portata dei bambini.'),
-(3, 'Cactus morbidoso (Rosso)', '10.00', '/TecnologieWeb/immagini/morbidosoRosso.png', 'Chi non vorrebbe abbracciare un cactus e sentirsi al sicuro da ogni pericolo? Ora puoi!\r\n\r\nATTENZIONE: non è garantita la totale assenza di spine, tenere lontano dalla portata dei bambini.'),
-(4, 'Bevanda Cac-tastica (0.5L)', '1.50', '/TecnologieWeb/immagini/bevandaMezza.png', 'Sei assetato e hai bisogno di energie immediatamente? Acquista la nostra nuova bevanda energetica per non rimanere mai a secco!\r\n\r\n'),
-(5, 'Bevanda Cac-tastica (1L)', '2.30', '/TecnologieWeb/immagini/bevandaLitro.png', 'Sei assetato e hai bisogno di energie immediatamente? Acquista la nostra nuova bevanda energetica per non rimanere mai a secco!\r\n'),
-(6, 'Mine-Cactus (Prodotto singolo)', '2.50', '/TecnologieWeb/immagini/cactusMC', 'Il cactus digitale diviene finalmente realtà! Come non volerlo aggiungere alla propria collezione di loli.\r\n\r\nConfezione singola.'),
-(7, 'Mine-Cactus (Full stack)', '100.00', '/TecnologieWeb/immagini/cactusMC', 'Il cactus digitale diviene finalmente realtà! Come non volerlo aggiungere alla propria collezione di loli.\r\n\r\nConfezione full stack (x64)'),
-(8, 'Cactus 404', '0.00', '/TecnologieWeb/immagini/null.png', 'Il cactus introvabile: ordinalo a tuo rischio e pericolo.\r\n\r\nNon garantiamo rimborsi in caso di mancato ricevimento di questo articolo.'),
-(9, 'Cactus', '1.00', '/TecnologieWeb/immagini/cactusNormal.png', 'E\' un cactus.'),
-(10, 'Cat-tus', '3.00', '/TecnologieWeb/immagini/cattus.png', 'Le due cose più adorabili che possano esistere in un unico pacchetto formato convenienza.');
+INSERT INTO `products` (`id`, `name`, `price`, `image`, `description`, `highlighted`) VALUES
+(1, 'Cactus con miccia', '2.50', '/TecnologieWeb/immagini/cactusMiccia.png', 'Cactus di porcellana attaccata ad una corda e pieno di polvere nera. Se lanciato alle spalle, può ostacolare gli inseguitori.\r\n\r\nE\' identico ad un comune cactus, tranne per la direzione in cui viene lanciato.', 1),
+(2, 'Cactus morbidoso (Verde)', '10.00', '/TecnologieWeb/immagini/morbidosoVerde.png', 'Chi non vorrebbe abbracciare un cactus e sentirsi al sicuro da ogni pericolo? Ora puoi!\r\n\r\nATTENZIONE: non è garantita la totale assenza di spine, tenere lontano dalla portata dei bambini.', 0),
+(3, 'Cactus morbidoso (Rosso)', '10.00', '/TecnologieWeb/immagini/morbidosoRosso.png', 'Chi non vorrebbe abbracciare un cactus e sentirsi al sicuro da ogni pericolo? Ora puoi!\r\n\r\nATTENZIONE: non è garantita la totale assenza di spine, tenere lontano dalla portata dei bambini.', 0),
+(4, 'Bevanda Cac-tastica (0.5L)', '1.50', '/TecnologieWeb/immagini/bevandaMezza.png', 'Sei assetato e hai bisogno di energie immediatamente? Acquista la nostra nuova bevanda energetica per non rimanere mai a secco!\r\n\r\n', 1),
+(5, 'Bevanda Cac-tastica (1L)', '2.30', '/TecnologieWeb/immagini/bevandaLitro.png', 'Sei assetato e hai bisogno di energie immediatamente? Acquista la nostra nuova bevanda energetica per non rimanere mai a secco!\r\n', 0),
+(6, 'Mine-Cactus (Prodotto singolo)', '2.50', '/TecnologieWeb/immagini/cactusMC', 'Il cactus digitale diviene finalmente realtà! Come non volerlo aggiungere alla propria collezione di loli.\r\n\r\nConfezione singola.', 0),
+(7, 'Mine-Cactus (Full stack)', '100.00', '/TecnologieWeb/immagini/cactusMC', 'Il cactus digitale diviene finalmente realtà! Come non volerlo aggiungere alla propria collezione di loli.\r\n\r\nConfezione full stack (x64)', 0),
+(8, 'Cactus 404', '0.00', '/TecnologieWeb/immagini/null.png', 'Il cactus introvabile: ordinalo a tuo rischio e pericolo.\r\n\r\nNon garantiamo rimborsi in caso di mancato ricevimento di questo articolo.', 1),
+(9, 'Cactus', '1.00', '/TecnologieWeb/immagini/cactusNormal.png', 'E\' un cactus.', 0),
+(10, 'Cat-tus', '3.00', '/TecnologieWeb/immagini/cattus.png', 'Le due cose più adorabili che possano esistere in un unico pacchetto formato convenienza.', 0);
 
 --
 -- Indici per le tabelle scaricate
