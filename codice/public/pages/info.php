59d025bda87a4182be10b306e24cdfd3cf961ab3
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2022-02-08 16:03:46 +0100

    info e about us fully done

--- codice/public/pages/info.php
+++ codice/public/pages/info.php
@@ -1,13 +1,30 @@
 <h1>Informazioni e contatti</h1>
-<article id="generali">
-    <h2>Informazioni generali:</h2>
-</article>
-<article id="trattamentoDati">
-    <h2>Politche di trattamento dei dati:</h2>
-</article>
-<article id="cookiePolicy">
-    <h2>Cookie policy:</h2>
-</article>
-<article id="contatti">
-    <h2>Contatti:</h2>
-</article>
+<section id="about">
+    <article id="generali">
+        <h2>Informazioni generali:</h2>
+        <h3>Consenso dell'utente:</h3>
+            <p>Collegandosi per la prima volta ad una qualunque pagina del nostro sito, l’utente vedrà apparire una sintetica informativa sull’utilizzo dei cookie. Non sarà possibile procedere con la navigazione sul sito prima di averne compreso l'utilizzo. Il cliente potrà anche visionare la policy completa in questa pagina, dove saranno visibili le varie categorie di cookie utilizzate, e potrà vedere nel dettaglio tutti i cookie utilizzati.
+            Qualora si riscontrassero problemi di natura tecnica legati alla prestazione del consenso, vi preghiamo di contattarci tramite gli appositi canali previsti da questo sito per consentici di prestarvi assistenza. Per i contatti vedi la sezione <a href="#contatti">contatti</a> sottostante.</p>
+    </article>
+    <article id="trattamentoDati">
+        <h2>Politche di trattamento dei dati:</h2>
+            <p>Questo sito, affinché possa funzionare correttamente e garantire al cliente una esperienza ottimale e soddisfacente, sfrutta il meccanismo dei <a href="#cookiePolicy">cookie</a>. Questi sono espressamente di natura tecnica e servono solamente a garantire il corretto funzionamento delle varie componenti del sito (carrello, login, etc .. ).</p>
+            <p>Non attuiamo profilazione né ci affidiamo ad aziende di terze parti per la loro gestione o per far funzionare i nostri servizi. Infine, non adottiamo sistemi di <em>analytics</em> per quanto riguarda le vendite dei prodotti.</p>
+    </article>
+    <article id="cookiePolicy">
+        <h2>Cookie policy:</h2>
+            <h3>Cosa sono i cookie:</h3>
+                <p>Il sito utilizza cookie. Come chiarito dal Garante Privacy nel proprio <a href="https://www.garanteprivacy.it/">sito</a>, i cookie sono informazioni immesse sul tuo browser quando visiti un sito web o utilizzi un social network con il tuo pc, smartphone o tablet.</p><br/>
+                <p>Ogni cookie contiene diversi dati come, ad esempio, il nome del server da cui proviene, un identificatore numerico, ecc..</p>
+                <p>I cookie possono rimanere nel sistema per la durata di una sessione (cioè fino a che si chiude il browser utilizzato per la navigazione sul web) o per lunghi periodi e possono contenere un codice identificativo univoco. Alcuni cookie sono usati per eseguire autenticazioni informatiche, monitoraggio di sessioni e memorizzazione di informazioni specifiche sugli utenti che accedono ad una pagina web.</p><br/>
+                <p>Questi cookie, cosiddetti <span>tecnici</span>, sono spesso utili, perché possono rendere più veloce e rapida la navigazione e fruizione del web, perché ad esempio intervengono a facilitare alcune procedure quando fai acquisti online, quando ti autentichi ad aree ad accesso riservato o quando un sito web riconosce in automatico la lingua che utilizzi di solito.
+                <span>I cookie tecnici sono necessari per il funzionamento del sito e non possono essere disattivati nei nostri sistemi</span>.</p><br/>
+            <h3>Cookie tecnici:</h3>
+                <p>
+                Questi cookie sono necessari per il funzionamento del sito e non possono essere disattivati nei nostri sistemi. Di solito vengono impostati solo in risposta alle azioni da te effettuate che costituiscono una richiesta di servizi, come l'impostazione delle preferenze di privacy, l'accesso o la compilazione di moduli. È possibile impostare il browser per bloccare o avere avvisi riguardo questi cookie, ma di conseguenza alcune parti del sito non funzioneranno. <span>Questi cookie non archiviano informazioni personali</span>.</p>
+    </article>
+    <article id="contatti">
+        <h2>Contatti:</h2>
+        <p>Tutte le comunicazioni in merito ad eventuali dubbi, perplessità, problemi di natura tecnica, recensioni o anche per soli complimenti avvengono via mail presso l'indirizzo email <span id="admin">admin@cacplusinc.com</span></p>
+    </article>
+</section>
