f8a3d4267d2e7cf9505dad86afe8f05abc964c51
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2022-02-08 15:05:35 +0100

    basics info and thinking about registration page, need to do css

--- codice/public/pages/info.php
+++ codice/public/pages/info.php
@@ -1,13 +1,13 @@
 <h1>Informazioni e contatti</h1>
-<section id="generali">
-
-</section>
-<section id="trattamentoDati">
-
-</section>
-<section id="cookiePolicy">
-
-</section>
-<section id="contatti">
-    
-</section>
+<article id="generali">
+    <h2>Informazioni generali:</h2>
+</article>
+<article id="trattamentoDati">
+    <h2>Politche di trattamento dei dati:</h2>
+</article>
+<article id="cookiePolicy">
+    <h2>Cookie policy:</h2>
+</article>
+<article id="contatti">
+    <h2>Contatti:</h2>
+</article>
--- codice/public/pages/login.php
+++ codice/public/pages/login.php
@@ -7,6 +7,10 @@
             </fieldset>
         </form>
     </section>
+    <section id="registration">
+        <h2>Non sei ancora registrato? Registra ora senza costi aggiuntivi!</h2>
+            <a href="<?php echo ROOT_URL;?>shop?page=registrazione">Registrazione</a>
+    </section>
     <section id="trattamento">
         <h2>Sul trattamento dei dati:</h2>
         <p>Questo sito web utilizza cookie di tipo tecnico (per consentire il corretto funzionamento del sito e rendere più rapido e migliore il suo utilizzo). Non usufruiamo di ulteriori strumenti o cookie di diverso tipo da quelli sopracitati, ne ci affidiamo ad aziende di terze parti per la loro gestione o per poter garantire il corretto funzionamento del sito web.</p>
