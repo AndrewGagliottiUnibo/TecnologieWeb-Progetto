567f616ab3ff43ecc4a2b50cdb20d51ce52b24cf
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2022-04-28 20:19:02 +0200

    quick fix

--- sito/public/pages/login.php
+++ sito/public/pages/login.php
@@ -6,38 +6,23 @@ if(isset($_GET['error'])) {
    echo 'Error Logging In!';
 }
 ?>
-<?php if(login_check($mysqli) == false) :?>
-    <form action="?page=process_login" method="post" name="login_form">
-    Email: <input type="text" name="email" /><br />
-    Password: <input type="password" name="p" id="password"/><br />
-    <input type="button" value="Login" onclick="formhash(this.form, this.form.password);" />
-    </form>
-
-
-<?php else: ?>
-<form action="?page=logout" method="post" name="logout_form">
-   <input type="submit" value="Logout"/>
-</form>
-
-<?php endif; ?>
-
-
 
 <h1>Sezione di accesso all'account personale</h1>
-    <section id="accessPoint">
-        <form method="POST">
-            <div id="container">
-                <h2>Inserisci qui i tuoi dati personali</h2>
-            </div>
-            <div id="email">
-                <label for="email">Email: <input type="email" name="email"></label>
-            </div>
-            <div id="input">
-                <label for="email">Password: <input type="password" name="email"></label>
-            </div>
-            <button type="submit" name="login">login</button>
+    <?php if(login_check($mysqli) == false) :?>
+        <form action="?page=process_login" method="post" name="login_form">
+        Email: <input type="text" name="email" /><br />
+        Password: <input type="password" name="p" id="password"/><br />
+        <input type="button" value="Login" onclick="formhash(this.form, this.form.password);" />
         </form>
-    </section>
+
+
+    <?php else: ?>
+    <form action="?page=logout" method="post" name="logout_form">
+    <input type="submit" value="Logout"/>
+    </form>
+
+    <?php endif; ?>
+    
     <section id="about">
         <article id="registration">
             <h2>Non sei ancora registrato? Registrati ora senza costi aggiuntivi!</h2>
