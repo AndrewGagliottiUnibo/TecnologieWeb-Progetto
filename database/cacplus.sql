07f38eec0115d61c889929d477ea22bf1f34ba7f
SalvatoreBennici <salvatore.bennici@studio.unibo.it>
2022-05-14 15:41:29 +0200

    Adding "admin" users type

--- database/cacplus.sql
+++ database/cacplus.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Apr 28, 2022 alle 17:55
+-- Creato il: Mag 14, 2022 alle 15:40
 -- Versione del server: 10.4.24-MariaDB
 -- Versione PHP: 7.3.33
 
@@ -38,8 +38,30 @@ CREATE TABLE `cart` (
 --
 
 INSERT INTO `cart` (`id`, `user_id`, `client_id`) VALUES
-(14, 0, '67c4b70d52b5205391d7'),
-(15, 0, '5e2182419da7d8ed7c2c');
+(36, 0, '4fac1085053e89659bc5'),
+(37, 0, '634462d2b6214478b5e7'),
+(38, 0, '15791a5f76dbe98c9af2'),
+(39, 0, 'dc8496a0bb20a6571011'),
+(40, 0, 'e8b7b8111000b7ec6730'),
+(41, 0, '9cd82b1c9818072eac99'),
+(42, 0, 'd48a34413d7271632316'),
+(43, 0, '0a488883e878bc1b1285'),
+(44, 0, '558269ea07832a9dbd5b'),
+(45, 0, '953984251696b17b50b2'),
+(46, 0, '0ecca5e00ded6fdca789'),
+(47, 0, '22c8a1921abb2cdc2483'),
+(48, 0, '26734ddc6ce1def4da34'),
+(49, 0, '53956c38449372894ca1'),
+(50, 0, '8eadf1f0bc2514a3400b'),
+(51, 0, '84a8211c661b8fdb1885'),
+(52, 0, 'cf299665c5e6cdc4fd86'),
+(53, 0, 'd2808de110701d188080'),
+(54, 0, '9f555e0bdfdcbca79367'),
+(55, 0, 'eb7dc184599f11404c4a'),
+(56, 0, '3f168b2b2ab16101f031'),
+(57, 0, ''),
+(58, 0, '8'),
+(59, 0, '10');
 
 -- --------------------------------------------------------
 
@@ -59,9 +81,7 @@ CREATE TABLE `cart_item` (
 --
 
 INSERT INTO `cart_item` (`id`, `cart_id`, `product_id`, `quantity`) VALUES
-(16, 14, 1, 1),
-(17, 14, 4, 1),
-(18, 15, 1, 8);
+(33, 59, 6, 1);
 
 -- --------------------------------------------------------
 
@@ -82,8 +102,7 @@ INSERT INTO `login_attempts` (`user_id`, `time`) VALUES
 (4, '1651069886'),
 (5, '1651069920'),
 (6, '1651070020'),
-(7, '1651070459'),
-(9, '1651070773');
+(7, '1651070459');
 
 -- --------------------------------------------------------
 
@@ -93,6 +112,7 @@ INSERT INTO `login_attempts` (`user_id`, `time`) VALUES
 
 CREATE TABLE `members` (
   `id` int(11) NOT NULL,
+  `admin` tinyint(1) NOT NULL DEFAULT 0,
   `username` varchar(30) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` char(128) NOT NULL,
@@ -103,11 +123,11 @@ CREATE TABLE `members` (
 -- Dump dei dati per la tabella `members`
 --
 
-INSERT INTO `members` (`id`, `username`, `email`, `password`, `salt`) VALUES
-(1, 'test_user', 'test@example.com', '00807432eae173f652f2064bdca1b61b290b52d40e429a7d295d76a71084aa96c0233b82f1feac45529e0726559645acaed6f3ae58a286b9f075916ebf66cacc', 'f9aab579fc1b41ed0c44fe4ecdbfcdb4cb99b9023abb241a6db833288f4eea3c02f76e0d35204a8695077dcf81932aa59006423976224be0390395bae152d4ef'),
-(8, 'gatto', 'gatto@gatto.it', 'd43dbe911bd2bd9c107e5eb349bae3b09121fc06bc7f3d8432fab1bb0a7346defc1befa7f6558b6fcb146c213ab9898fdfc85b8d397ff6217c61f79bbc5c126f', 'd57b3c69336aea54debc6a5da783913243db1d94d172ede46366b645ff161aa401ace83ae3f51aa6ae6dbe2a4e87cbd65a58f4242b2d4e8c1146faeb1198f92b'),
-(9, 'cane', 'cane@cane.it', 'ec35db0ae3bc6ba83c00500b50b7d4d1a686be95059f398ab3d505d3847f605181da351eef288366175d3d52b422cdb56548a2bc6e08d2d35b8ff0266c1e3ee0', '0c274891afee3f0723383a04b6e19faca02c9b66bd125cd627397dbbc19fa14b3c23a4db43d4bf200c419b29c5252d01eec2d459942b5625925e9b0a46f414c0'),
-(10, 'mago', 'mago@mago.it', 'f07afe777001339f9b7763078bc4222e3e5d69a48676b58ed3b24f49dc43fa0a439ec9358896f2ae9413e2c980550132e255e37d3dd0a5223ba1d3279738c636', '9c508ef6ebc80505b507810b6d02241b41d8afb3d30cf13687052e32f76bf9a26af4a5653f728b5de8ab8a9859d10aa26ecedd8335c11b99b7c4d42678918ce4');
+INSERT INTO `members` (`id`, `admin`, `username`, `email`, `password`, `salt`) VALUES
+(1, 0, 'test_user', 'test@example.com', '00807432eae173f652f2064bdca1b61b290b52d40e429a7d295d76a71084aa96c0233b82f1feac45529e0726559645acaed6f3ae58a286b9f075916ebf66cacc', 'f9aab579fc1b41ed0c44fe4ecdbfcdb4cb99b9023abb241a6db833288f4eea3c02f76e0d35204a8695077dcf81932aa59006423976224be0390395bae152d4ef'),
+(8, 0, 'gatto', 'gatto@gatto.it', 'd43dbe911bd2bd9c107e5eb349bae3b09121fc06bc7f3d8432fab1bb0a7346defc1befa7f6558b6fcb146c213ab9898fdfc85b8d397ff6217c61f79bbc5c126f', 'd57b3c69336aea54debc6a5da783913243db1d94d172ede46366b645ff161aa401ace83ae3f51aa6ae6dbe2a4e87cbd65a58f4242b2d4e8c1146faeb1198f92b'),
+(9, 0, 'cane', 'cane@cane.it', 'ec35db0ae3bc6ba83c00500b50b7d4d1a686be95059f398ab3d505d3847f605181da351eef288366175d3d52b422cdb56548a2bc6e08d2d35b8ff0266c1e3ee0', '0c274891afee3f0723383a04b6e19faca02c9b66bd125cd627397dbbc19fa14b3c23a4db43d4bf200c419b29c5252d01eec2d459942b5625925e9b0a46f414c0'),
+(10, 1, 'mago', 'mago@mago.it', 'f07afe777001339f9b7763078bc4222e3e5d69a48676b58ed3b24f49dc43fa0a439ec9358896f2ae9413e2c980550132e255e37d3dd0a5223ba1d3279738c636', '9c508ef6ebc80505b507810b6d02241b41d8afb3d30cf13687052e32f76bf9a26af4a5653f728b5de8ab8a9859d10aa26ecedd8335c11b99b7c4d42678918ce4');
 
 -- --------------------------------------------------------
 
@@ -178,13 +198,13 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT per la tabella `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
 
 --
 -- AUTO_INCREMENT per la tabella `cart_item`
 --
 ALTER TABLE `cart_item`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT per la tabella `members`
--- sito/public/pages/process_login.php
+++ sito/public/pages/process_login.php
@@ -1,17 +1,29 @@
 <?php
 include_once ROOT_PATH . "/sito/script/functions.php";
 require_once ROOT_PATH . "/sito/inc/init.php";
-if(isset($_POST['email'], $_POST['p'])) { 
+if (isset($_POST['email'], $_POST['p'])) {
    $email = $_POST['email'];
    $password = $_POST['p']; // Recupero la password criptata.
-   if(login($email, $password, $mysqli) == true) {
-      // Login eseguito
-      echo 'Success: You have been logged in!';
+   if (login($email, $password, $mysqli) == true) {
+      // Login eseguito, controlla se admin
+      $admin;
+      $stmt = $mysqli->prepare("SELECT admin FROM members WHERE email = ? LIMIT 1");
+      $stmt->bind_param('s', $email); // esegue il bind del parametro '$email'.
+      $stmt->execute(); // esegue la query appena creata.
+      $stmt->store_result();
+      $stmt->bind_result($admin); // recupera il risultato della query e lo memorizza nelle relative variabili.
+      $stmt->fetch();
+
+      if ($admin) {
+         echo 'Success: You have been logged in as an Admin!';
+         //redirect to admin page
+      } else {
+         echo 'Success: You have been logged in!';
+      }
    } else {
       // Login fallito
       echo 'Unsuccess login';
    }
-} else { 
+} else {
    // Le variabili corrette non sono state inviate a questa pagina dal metodo POST.
 }
-?>
