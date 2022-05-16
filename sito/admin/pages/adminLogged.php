f6f25e363c07bfe9e046470d976cef03e4138339
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2022-05-16 10:20:23 +0200

    manual merge for backend and frontend

---
+++ sito/admin/pages/adminLogged.php
@@ -0,0 +1,35 @@
+<?php
+if (isset($_POST['add_new_product'])) {
+    header("location:" . ROOT_URL . "admin?page=add_product");
+}
+
+if (isset($_POST['modify_product'])) {
+    header("location:" . ROOT_URL . "admin?page=modify_product");
+}
+
+if (isset($_POST['remove_product'])) {
+    header("location:" . ROOT_URL . "admin?page=remove_product");
+}
+?>
+
+<h2 class="adminWelcome">Bentornato!</h2>
+<div class="dbOperations">
+    <h3 class="adminOperation">Aggiungi prodotti</h2>
+        <p class="operationDescription">
+            Inserimento di prodotti nel catalogo
+        </p>
+        <a name="add_new_product" class="gotoSpecific" href="#">Prosegui</a>
+                        
+    <h3 class="adminOperation">Modifica prodotti</h3>
+        <p class="operationDescription">
+            Modifica di nome, prezzo, descrizione di ogni elemento del catalogo
+        </p>
+        <a name="modify_product" class="gotoSpecific" href="#">Prosegui</a>
+    
+    <h3 class="adminOperation">Rimuovi prodotti</h3>
+        <p class="operationDescription">
+            Rimozione di un elemento dal catalogo
+        </p>
+        <a name="remove_product" class="gotoSpecific" href="#">Prosegui</a>
+</div>
+    
