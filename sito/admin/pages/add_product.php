484cb8be7ae0a27979376d7623ce65d5085365c2
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2022-05-16 12:40:32 +0200

    file input

--- sito/admin/pages/add_product.php
+++ sito/admin/pages/add_product.php
@@ -1,7 +1,7 @@
 <form action="?page=process_add_product" method="post">
     Nome: <input type="text" name="name"><br>
     Prezzo: <input type="number" step="0.01" name="price"><br>
-    Immagine: <input type="text" name="image"><br>
+    Immagine: <input type="file" name="image"><br>
     Descrizione: <input type="text" name="description"><br>
     <input type="submit">
 </form>
