f7993f97f8e882ff07f58e441c6f99b81edd4a23
SalvatoreBennici <salvatore.bennici@studio.unibo.it>
2022-05-17 23:18:27 +0200

    Fixing modify data from user

--- sito/public/pages/process_modify_data.php
+++ sito/public/pages/process_modify_data.php
@@ -2,30 +2,42 @@
 require_once ROOT_PATH . "/sito/inc/init.php";
 require_once ROOT_PATH . "/sito/script/functions.php";
 
-if (isset($_POST['oldPassword'], $_POST['password'],  $_POST['confirm'])) {
+if (isset($_POST['oldPassword'], $_POST['newPassword'],  $_POST['confirm'])) {
     $usrId = $_SESSION['user_id'];
     $oldPassword = $_POST['oldPassword'];
-    $password = $_POST['password'];
+    $newPassword = $_POST['newPassword'];
     $confirm = $_POST['confirm'];
+    var_dump($oldPassword);
+    var_dump($newPassword);
 
-    //Passowrd selection
-    $stmt = $mysqli->prepare("SELECT password FROM members WHERE id = ?");
+    //  Ottengo la password attuale
+    $stmt = $mysqli->prepare("SELECT email, password, salt FROM members WHERE id = ? LIMIT 1");
     $stmt->bind_param('i', $usrId);
     $stmt->execute();
-    $result = $stmt->get_result();
-    $check =  mysqli_fetch_array($result)['password'];
+    $result = mysqli_fetch_array($stmt->get_result());
+    $email =  $result['email'];
+    $salt =  $result['salt'];
+    $db_password =  $result['password'];
+    $check = hash('sha512', $oldPassword . $salt);
 
-    var_dump($check);
-    var_dump($oldPassword);
-    var_dump($password);
-    // Check if product already exist
-    if ($check === $oldPassword) {
-        var_dump($check);
-        //if ($password === $confirm) {
-            $stmt = $mysqli->prepare("UPDATE members SET password = ? WHERE id = ?");
-            $stmt->bind_param('si', $password, $usrId);
-            $stmt->execute();
-            echo('tutto ok');
-        //}
+    // controllo se la vecchia password coincide con quella attuale
+    if ($check === $db_password) {
+        //Codfico la nuova pass
+        $random_salt = hash('sha512', uniqid(mt_rand(1, mt_getrandmax()), true));
+        $newPassword = hash('sha512', $newPassword . $random_salt);
+
+        $stmt = $mysqli->prepare("UPDATE members SET password = ?, salt = ? WHERE id = ?");
+        $stmt->bind_param('ssi', $newPassword, $random_salt, $usrId);
+        $stmt->execute();
+
+        // logout
+        $_SESSION = array();
+        // Recupera i parametri di sessione.
+        $params = session_get_cookie_params();
+        // Cancella i cookie attuali.
+        setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
+        // Cancella la sessione.
+        session_destroy();
+        login($email, $confirm, $mysqli);
     }
 }
--- sito/script/forms.js
+++ sito/script/forms.js
@@ -1,36 +1,46 @@
 function formhash(form, password) {
-    // Crea un elemento di input che verrà usato come campo di output per la password criptata.
-    var p = document.createElement("input");
-    // Aggiungi un nuovo elemento al tuo form.
-    form.appendChild(p);
-    p.name = "p";
-    p.type = "hidden"
-    p.value = hex_sha512(password.value);
-    // Assicurati che la password non venga inviata in chiaro.
-    password.value = "";
-    // Come ultimo passaggio, esegui il 'submit' del form.
-    form.submit();
+  // Crea un elemento di input che verrà usato come campo di output per la password criptata.
+  var p = document.createElement("input");
+  // Aggiungi un nuovo elemento al tuo form.
+  form.appendChild(p);
+  p.name = "p";
+  p.type = "hidden"
+  p.value = hex_sha512(password.value);
+  // Assicurati che la password non venga inviata in chiaro.
+  password.value = "";
+  // Come ultimo passaggio, esegui il 'submit' del form.
+  form.submit();
 }
 
-function formModifyHash(form, password, confirm) {
-    // Crea un elemento di input che verrà usato come campo di output per la password criptata.
-    var p = document.createElement("input");
-    // Aggiungi un nuovo elemento al tuo form.
-    form.appendChild(p);
-    p.name = "password";
-    p.type = "hidden"
-    p.value = hex_sha512(password.value);
-    // Assicurati che la password non venga inviata in chiaro.
-    password.value = "";
-    // Come ultimo passaggio, esegui il 'submit' del form.
-    form.submit();
-}
-
-var check = function() {
-    if (document.getElementById('password_field').value ==
-      document.getElementById('confirmation_field').value) {
-      document.getElementById('message').innerHTML = 'matching';
-    } else {
-      document.getElementById('message').innerHTML = 'not matching';
-    }
+function formModifyHash(form, old_Password, new_Password, confirm_Password) {
+  // Crea un elemento di input che verrà usato come campo di output per la password criptata.
+  if (new_Password.value !== confirm_Password.value) {
+    return;
   }
+  var oldPassword = document.createElement("input");
+  var newPassword = document.createElement("input");
+  // Aggiungi un nuovo elemento al tuo form.
+  form.appendChild(oldPassword);
+  form.appendChild(newPassword);
+  oldPassword.name = "oldPassword";
+  oldPassword.type = "hidden"
+  oldPassword.value = hex_sha512(old_Password.value);
+  newPassword.name = "newPassword";
+  newPassword.type = "hidden"
+  newPassword.value = hex_sha512(new_Password.value);
+  // Assicurati che la password non venga inviata in chiaro.
+  old_Password.value = "old";
+  new_Password.value = "new";
+  // Come ultimo passaggio, esegui il 'submit' del form.
+
+  form.submit();
+}
+
+var check = function () {
+  if (document.getElementById('password_field').value ==
+    document.getElementById('confirmation_field').value) {
+    document.getElementById('message').innerHTML = 'matching';
+  } else {
+    document.getElementById('message').innerHTML = 'not matching';
+  }
+}
