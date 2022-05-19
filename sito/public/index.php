6ab69d915e120e9c8ea6a6e28106ffb2b4ad50eb
SalvatoreBennici <salvatore.bennici@studio.unibo.it>
2022-05-19 12:46:55 +0200

    Adding css to notifiche

--- sito/public/index.php
+++ sito/public/index.php
@@ -12,6 +12,7 @@ $page = isset($_GET["page"]) ? $_GET["page"] : "homepage";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CacPlus.inc</title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="../css/style.css">
 </head>
 
--- sito/public/pages/notification.php
+++ sito/public/pages/notification.php
@@ -2,25 +2,36 @@
 $table = "notifications";
 if (isAdmin($mysqli)) {
     $table = "notifications_admin";
+    $stmt = $mysqli->prepare("SELECT * FROM " . $table);
+} else {
+    $id = $_SESSION['user_id'];
+    $stmt = $mysqli->prepare("SELECT * FROM " . $table . " WHERE  user = ?");
+    $stmt->bind_param('i', $id);
 }
 
-$stmt = $mysqli->prepare("SELECT * FROM " . $table);
 $stmt->execute(); // esegue la query appena creata.
 $result = $stmt->get_result();
 
 ?>
 
-
-<table>
-    <caption>Notifiche</caption>
-    <tr>
-        <th scope="col">Messaggio</th>
-        <th scope="col">Data</th>
-    </tr>
-    <?php while ($row = mysqli_fetch_array($result)) { ?>
+<section>
+    <table>
+        <caption>Notifiche</caption>
         <tr>
-            <td><?php echo $row['message']; ?></td>
-            <td><?php echo $row['datetime']; ?></td>
+            <th scope="col">Messaggio</th>
+            <th scope="col">Data</th>
+            <th><button class="material-icons">
+                    delete_sweep
+                </button></th>
         </tr>
-    <?php } ?>
-</table>
+        <?php while ($row = mysqli_fetch_array($result)) { ?>
+            <tr>
+                <td><?php echo $row['message']; ?></td>
+                <td><?php echo $row['datetime']; ?></td>
+                <td><button class="material-icons">
+                        delete
+                    </button></td>
+            </tr>
+        <?php } ?>
+    </table>
+</section>
