622b5ad7910b76bfbbaa51a9850356655b71990c
SalvatoreBennici <salvatore.bennici@studio.unibo.it>
2022-05-19 17:52:24 +0200

    Adding dynamic loading to notifiche

--- sito/public/index.php
+++ sito/public/index.php
@@ -23,12 +23,11 @@ $page = isset($_GET["page"]) ? $_GET["page"] : "homepage";
     </main>
     <?php include ROOT_PATH . "/sito/template/footer.php"; ?>
 </body>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script type="text/javascript" src="../script/navbar.js"></script>
 <script type="text/javascript" src="../script/card.js"></script>
 <script type="text/javascript" src="../script/sha512.js"></script>
+<script type="text/javascript" src="../script/listeners.js"></script>
 <script type="text/javascript" src="../script/forms.js"></script>
-<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
-
 
 </html>
