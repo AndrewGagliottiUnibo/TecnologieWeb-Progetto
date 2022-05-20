0824d42ed9638752dbb4e4ea14f46914be8d0646
SalvatoreBennici <salvatore.bennici@studio.unibo.it>
2022-05-20 12:17:17 +0200

    Delete user folder

--- sito/script/listeners.js
+++ sito/script/listeners.js
@@ -1,7 +1,7 @@
 /**
  * Button listeners for notifications table
  */
- $(document).ready(function () {
+$(document).ready(function () {
     $('body').on('click', '#notifications_table button', function () {
         var ntf_id = $(this).attr('name');
         $("#notifications_section").load("?page=load_notifications #notifications_table", {
