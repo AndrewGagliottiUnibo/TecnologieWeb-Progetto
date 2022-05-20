a0873b13ba2becea37a37848dd791fb2050830cb
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2022-05-20 18:16:07 +0200

    admin design

--- sito/admin/pages/admin_logged.php
+++ sito/admin/pages/admin_logged.php
@@ -16,30 +16,37 @@ if (isset($_POST['update-data'])) {
 }
 ?>
 
-<h2 class="adminWelcome">Bentornato!</h2>
+<h2 class="cool_title">Bentornato!</h2>
 <div class="dbOperations">
-    <h3 class="adminOperation">Aggiungi prodotti</h2>
-        <p class="operationDescription">
-            Inserimento di prodotti nel catalogo
-        </p>
-        <a name="add_new_product" class="gotoSpecific" href=<?php echo ROOT_URL . "admin?page=add_product"?>>Prosegui</a>
-                        
-    <h3 class="adminOperation">Modifica prodotti</h3>
-        <p class="operationDescription">
-            Modifica di nome, prezzo, descrizione di ogni elemento del catalogo
-        </p>
-        <a name="modify_product" class="gotoSpecific" href=<?php echo ROOT_URL . "admin?page=modify_product"?> >Prosegui</a>
-    
-    <h3 class="adminOperation">Rimuovi prodotti</h3>
-        <p class="operationDescription">
-            Rimozione di un elemento dal catalogo
-        </p>
-        <a name="remove_product" class="gotoSpecific" href=<?php echo ROOT_URL . "admin?page=remove_product"?> >Prosegui</a>
+        <div class="separator">
+            <h3 class="adminOperation">Aggiungi prodotti</h2>
+                <p class="operationDescription">
+                    Inserimento di prodotti nel catalogo
+                </p>
+                <a name="add_new_product" id="btn-admin" class="commit_button" href=<?php echo ROOT_URL . "admin?page=add_product"?>>Prosegui</a>
+        </div>
+        
+        <div class="separator">
+            <h3 class="adminOperation">Modifica prodotti</h3>
+                <p class="operationDescription">
+                    Modifica di nome, prezzo, descrizione di ogni elemento del catalogo
+                </p>
+                <a name="modify_product" id="btn-admin" class="commit_button" href=<?php echo ROOT_URL . "admin?page=modify_product"?> >Prosegui</a>
+        </div> 
 
-    <h3 class="adminOperation">Aggiorna le credenziali</h3>
-        <p class="operationDescription">
-            Aggiorna le tue e-mail e password
-        </p>
-        <a name="update-data" class="gotoSpecific" href=<?php echo ROOT_URL . "public?page=modify_data"?> >Prosegui</a>
+        <div class="separator">
+            <h3 class="adminOperation">Rimuovi prodotti</h3>
+                <p class="operationDescription">
+                    Rimozione di un elemento dal catalogo
+                </p>
+                <a name="remove_product" id="btn-admin" class="commit_button" href=<?php echo ROOT_URL . "admin?page=remove_product"?> >Prosegui</a>
+        </div>
+        <div class="separator">
+            <h3 class="adminOperation">Aggiorna le credenziali</h3>
+                <p class="operationDescription">
+                    Aggiorna le tue e-mail e password
+                </p>
+                <a name="update-data" id="btn-admin" class="commit_button" href=<?php echo ROOT_URL . "public?page=modify_data"?> >Prosegui</a>
+        </div>
 </div>
     
