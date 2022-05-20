895bb0a8346d6137818d504bc662d250f4f9af34
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2022-05-20 21:35:21 +0200

    js fix

--- sito/script/productHandler.js
+++ sito/script/productHandler.js
@@ -1,7 +1,9 @@
-$("#btn-add").click(function () {
-    alert('Aggiunto al carrello')
-});
-
-$("#btn-not-log").click(function () {
-    alert('Non hai effettuato il login. Accedi o Registrati ora per usufruire al meglio dell\'esperienza di Cacplus')
+$(document).ready(function () { 
+    $("#btn-add").click(function () {
+        alert('Aggiunto al carrello')
+    });
+    
+    $("#btn-not-log").click(function () {
+        alert('Non hai effettuato il login. Accedi o Registrati ora per usufruire al meglio dell\'esperienza di Cacplus')
+    });
 });
