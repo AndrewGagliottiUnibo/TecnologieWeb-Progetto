3c2062a65957df3e303c626132bb6f6aa1117e85
SalvatoreBennici <salvatore.bennici@studio.unibo.it>
2022-05-20 21:41:58 +0200

    fixing card script

--- sito/script/card.js
+++ sito/script/card.js
@@ -1,13 +1,15 @@
-cards = document.querySelectorAll('.card');
+$(document).ready(function () {
+    cards = document.querySelectorAll('.card');
 
-Array.prototype.forEach.call(cards, card => {
-    let down, up, link = card.querySelector('h4 a');
-    card.style.cursor = 'pointer';
-    card.onmousedown = () => down = +new Date();
-    card.onmouseup = () => {
-        up = +new Date();
-        if ((up - down) < 200) {
-            link.click();
+    Array.prototype.forEach.call(cards, card => {
+        let down, up, link = card.querySelector('h4 a');
+        card.style.cursor = 'pointer';
+        card.onmousedown = () => down = +new Date();
+        card.onmouseup = () => {
+            up = +new Date();
+            if ((up - down) < 200) {
+                link.click();
+            }
         }
-    }
+    });
 });
