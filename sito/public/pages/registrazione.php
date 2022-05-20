631f18d1e845bc9d4561ab02e02ac1f1677976bc
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2022-05-20 21:24:54 +0200

    css fix global

--- sito/public/pages/registrazione.php
+++ sito/public/pages/registrazione.php
@@ -15,7 +15,7 @@ require_once ROOT_PATH . "/sito/script/functions.php";
     <article id="trattamento">
         <h2>Sul trattamento dei dati:</h2>
         <p>Questo sito web utilizza cookie di tipo tecnico (per consentire il corretto funzionamento del sito e rendere più rapido e migliore il suo utilizzo). Non usufruiamo di ulteriori strumenti o cookie di diverso tipo da quelli sopracitati, ne ci affidiamo ad aziende di terze parti per la loro gestione o per poter garantire il corretto funzionamento del sito web.</p>
-        <p>Per quanto concerne i dettagli sulla privacy e il trattamento dei dati, vi rimandiamo alla sezione <a href="<?php echo ROOT_URL; ?>public?page=info">Info e Contatti</a>.</p>
+        <p>Per quanto concerne i dettagli sulla privacy e il trattamento dei dati, vi rimandiamo alla sezione <a class="info" href="<?php echo ROOT_URL; ?>public?page=info">Info e Contatti</a>.</p>
         <p> <span id="important">Registrandoti a questo sito dichiari di aver accettato la politica sul trattamento dei dati e sull'utilizzo di cookie tecnici per garantire il corretto funzionamento dei nostri servizi</span>.</p>
     </article>
     <aside id="pageFiller"></aside>
