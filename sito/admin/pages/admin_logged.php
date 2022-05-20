8cbc26a33ec0c6526468ba4a84003dd0034c4eac
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2022-05-20 20:15:43 +0200

    dummy error

--- sito/admin/pages/admin_logged.php
+++ sito/admin/pages/admin_logged.php
@@ -5,7 +5,7 @@
             <p class="operationDescription">
                 Inserimento di prodotti nel catalogo.
             </p>
-            <a id="btn-admin" class="commit_button" href="<?php echo ROOT_URL . "admin?page=add_product" ?>">Prosegui</a>
+            <a class="commit_button btn-admin" href="<?php echo ROOT_URL . "admin?page=add_product" ?>">Prosegui</a>
     </div>
 
     <div class="separator">
@@ -13,7 +13,7 @@
         <p class="operationDescription">
             Modifica dati di un prodotto nel catalogo.
         </p>
-        <a id="btn-admin" class="commit_button" href="<?php echo ROOT_URL . "admin?page=modify_product" ?>">Prosegui</a>
+        <a class="commit_button btn-admin" href="<?php echo ROOT_URL . "admin?page=modify_product" ?>">Prosegui</a>
     </div>
 
     <div class="separator">
@@ -21,13 +21,13 @@
         <p class="operationDescription">
             Rimozione di un elemento dal catalogo.
         </p>
-        <a id="btn-admin" class="commit_button" href="<?php echo ROOT_URL . "admin?page=remove_product" ?>">Prosegui</a>
+        <a class="commit_button btn-admin" href="<?php echo ROOT_URL . "admin?page=remove_product" ?>">Prosegui</a>
     </div>
     <div class="separator">
         <h3 class="adminOperation">Aggiorna le credenziali</h3>
         <p class="operationDescription">
             Aggiorna le tue e-mail e password.
         </p>
-        <a id="btn-admin" class="commit_button" href="<?php echo ROOT_URL . "public?page=modify_data" ?>">Prosegui</a>
+        <a class="commit_button btn-admin" href="<?php echo ROOT_URL . "public?page=modify_data" ?>">Prosegui</a>
     </div>
 </div>
