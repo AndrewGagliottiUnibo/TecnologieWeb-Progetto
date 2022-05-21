69c74518433813565acaa473de1b1a8d7dde240b
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2022-05-21 14:34:28 +0200

    icons for navbar

--- sito/template/navbar.php
+++ sito/template/navbar.php
@@ -14,27 +14,34 @@
     <div id="main-nav">
         <ul>
             <li>
+                <button class="material-icons" name="all">account_circle</button>
                 <a href="<?php echo ROOT_URL; ?>public?page=login">Profilo</a>
             </li>
             <?php if (isAdmin($mysqli)) : ?>
                 <li>
+                    <button class="material-icons" name="all">manage_accounts</button>
                     <a href="<?php echo ROOT_URL; ?>admin?page=admin_logged">Admin</a>
                 </li>
             <?php else : ?>
                 <li>
+                    <button class="material-icons" name="all">shopping_cart</button>
                     <a href="<?php echo ROOT_URL; ?>shop?page=cart">Carrello</a>
                 </li>
             <?php endif; ?>
             <li>
+                <button class="material-icons" name="all">shop_2</button>
                 <a class="collapsable" href="<?php echo ROOT_URL; ?>shop?page=catalogo">Catalogo</a>
             </li>
             <li>
+                <button class="material-icons" name="all">notifications</button>   
                 <a class="collapsable" href="<?php echo ROOT_URL; ?>public?page=notification">Notifiche</a>
             </li>
             <li>
+                <button class="material-icons" name="all">info</button>
                 <a class="collapsable" href="<?php echo ROOT_URL; ?>public?page=info">Info e Contatti</a>
             </li>
             <li>
+                <button class="material-icons" name="all">work_outline</button>
                 <a class="collapsable" href="<?php echo ROOT_URL; ?>public?page=chi_siamo">Chi Siamo</a>
             </li>
         </ul>
