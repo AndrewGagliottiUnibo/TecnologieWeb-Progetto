658370c921949fd5860f99b5a5ce710f77f28c75
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2022-05-21 15:17:32 +0200

    icons visual fix

--- sito/shop/index.php
+++ sito/shop/index.php
@@ -12,6 +12,7 @@ $page = isset($_GET["page"]) ? $_GET["page"] : "catalogo";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CacPlus.inc</title>
     <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
     <script src="../script/forms.js"></script>
