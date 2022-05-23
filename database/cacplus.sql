b7e9f04bae22c111899700b2a9f3c90b7cb52fce
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2022-05-23 17:48:44 +0200

    the last product, the real purpose for this website

--- database/cacplus.sql
+++ database/cacplus.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Mag 20, 2022 alle 17:40
+-- Creato il: Mag 23, 2022 alle 17:47
 -- Versione del server: 10.4.22-MariaDB
 -- Versione PHP: 7.3.33
 
@@ -87,7 +87,8 @@ CREATE TABLE `members` (
 --
 
 INSERT INTO `members` (`id`, `admin`, `username`, `email`, `password`, `salt`) VALUES
-(10, 1, 'mago', 'mago@mago.it', 'f07afe777001339f9b7763078bc4222e3e5d69a48676b58ed3b24f49dc43fa0a439ec9358896f2ae9413e2c980550132e255e37d3dd0a5223ba1d3279738c636', '9c508ef6ebc80505b507810b6d02241b41d8afb3d30cf13687052e32f76bf9a26af4a5653f728b5de8ab8a9859d10aa26ecedd8335c11b99b7c4d42678918ce4');
+(10, 1, 'mago', 'mago@mago.it', 'f07afe777001339f9b7763078bc4222e3e5d69a48676b58ed3b24f49dc43fa0a439ec9358896f2ae9413e2c980550132e255e37d3dd0a5223ba1d3279738c636', '9c508ef6ebc80505b507810b6d02241b41d8afb3d30cf13687052e32f76bf9a26af4a5653f728b5de8ab8a9859d10aa26ecedd8335c11b99b7c4d42678918ce4'),
+(12, 0, 'cliente', 'cliente1@cliente.com', '330e3e5df40c758175a60f25ef295985942c347f4cbc343cb7e9c02dc7fbeed98d10697d0cb51ae8d08194e3cb7b89f60a3a6c2460cd3526a64211e0196264aa', '9124ffc0edef1b678b7a3d432e6c3fb481a31a329b32de6b2d0a6d320803add5d9e1ae2a15f4c4019e62f4a31eee3198d64e136b2e0ed546ac89f356d92de71a');
 
 -- --------------------------------------------------------
 
@@ -137,7 +138,7 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `price`, `image`, `description`, `highlighted`) VALUES
-(1, 'Cactus con miccia', '2.50', 'cactusMiccia.png', 'Cactus di porcellana attaccata ad una corda e pieno di polvere nera. Se lanciato alle spalle, può ostacolare gli inseguitori.\r\n\r\nE\' identico ad un comune cactus, tranne per la direzione in cui viene lanciato.', 1),
+(1, 'Cactus con miccia', '2.50', 'cactusMiccia.png', 'Cactus di porcellana attaccata ad una corda e pieno di polvere nera. Se lanciato alle spalle, può ostacolare gli inseguitori.\r\n\r\nE\' identico ad un comune cactus, tranne per la direzione in cui viene lanciato.', 0),
 (2, 'Cactus morbidoso (Verde)', '10.00', 'morbidosoVerde.png', 'Chi non vorrebbe abbracciare un cactus e sentirsi al sicuro da ogni pericolo? Ora puoi!\r\n\r\nATTENZIONE: non è garantita la totale assenza di spine, tenere lontano dalla portata dei bambini.', 0),
 (3, 'Cactus morbidoso (Rosso)', '10.00', 'morbidosoRosso.png', 'Chi non vorrebbe abbracciare un cactus e sentirsi al sicuro da ogni pericolo? Ora puoi!\r\n\r\nATTENZIONE: non è garantita la totale assenza di spine, tenere lontano dalla portata dei bambini.', 0),
 (4, 'Bevanda Cac-tastica (0.5L)', '1.50', 'bevandaMezza.png', 'Sei assetato e hai bisogno di energie immediatamente? Acquista la nostra nuova bevanda energetica per non rimanere mai a secco!\r\n\r\n', 1),
@@ -150,7 +151,8 @@ INSERT INTO `products` (`id`, `name`, `price`, `image`, `description`, `highligh
 (11, 'Dipint-cactus', '15.00', 'cactusPaint.jpg', 'Dipinto a tema cactus che simboleggia l\'amore infinito e la devozione verso i cactus.', 0),
 (12, 'Composizione di cactus', '7.00', 'cactusComposito.jpg', 'Composizione di cactus di differenti origini.', 0),
 (13, 'Cactus parruccoso', '1.00', 'cactusParrucco.jpg', 'Cactus a forma di parrucca estraibile e indossabile.', 0),
-(14, 'Collan-actus', '350.00', 'cactusGioiello.jpg', 'Collana in argento che simboleggia la devozione verso i cactus.', 0);
+(14, 'Collan-actus', '350.00', 'cactusGioiello.jpg', 'Collana in argento che simboleggia la devozione verso i cactus.', 0),
+(15, 'Cactus sullo skate', '3.50', 'cactusSulloSkate.jpg', 'Nato dalla collaborazione epica finita male con l\'associazione nazionale per lo sport di Cactialandia.', 1);
 
 --
 -- Indici per le tabelle scaricate
@@ -202,37 +204,37 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT per la tabella `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=92;
 
 --
 -- AUTO_INCREMENT per la tabella `cart_item`
 --
 ALTER TABLE `cart_item`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=84;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=118;
 
 --
 -- AUTO_INCREMENT per la tabella `members`
 --
 ALTER TABLE `members`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT per la tabella `notifications`
 --
 ALTER TABLE `notifications`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
 
 --
 -- AUTO_INCREMENT per la tabella `notifications_admin`
 --
 ALTER TABLE `notifications_admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
 -- AUTO_INCREMENT per la tabella `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- Limiti per le tabelle scaricate
